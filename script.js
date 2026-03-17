f73d1ba7b7dcc6f74b5b473a9b9cd8f06d495196
sepherinamontgomery-collab <sepherinamontgomery@gmail.com>
2026-03-17 20:55:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const images = document.querySelectorAll("#portfolio img");
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+const closeBtn = document.getElementById("closeModal");
+
+images.forEach(img => {
+img.addEventListener("click", () => {
+modal.style.display = "flex";
+modalImg.src = img.src;
+});
+});
+
+closeBtn.onclick = () => {
+modal.style.display = "none";
+};
+
+modal.onclick = (e) => {
+if (e.target !== modalImg) {
+modal.style.display = "none";
+}
+};
 
