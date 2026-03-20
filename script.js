aeaf86d0847bfc85fd95334f6a27b64990ba433b
sepherinamontgomery-collab <sepherinamontgomery@gmail.com>
2026-03-20 22:15:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -22,17 +22,19 @@ modal.style.display = "none";
 
 const hero = document.getElementById("home");
 
-const images = [
+const heroImages = [
 "images/Boho Artwork Aesthetic Today's Mantra _20260219_102030_0000.png",
 "images/Orange and Black Illustrated Design Thinking Process Explained Poster_20260219_101928_0000.png",
 "images/Screenshot 2026-03-17 210250.png"
 ];
 
-let index = 0;
+let current = 0;
 
+/* SET FIRST IMAGE IMMEDIATELY */
+hero.style.backgroundImage = `url(${heroImages[current]})`;
+
+/* CHANGE EVERY 3 SECONDS */
 setInterval(() => {
-index = (index + 1) % images.length;
-hero.style.backgroundImage = `url(${images[index]})`;
+current = (current + 1) % heroImages.length;
+hero.style.backgroundImage = `url(${heroImages[current]})`;
 }, 3000);
-
-
